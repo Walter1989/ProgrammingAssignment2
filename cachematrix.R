abe58dfbe9159544290da51b4754c92e958e9769
Walter1989 <w.s.hoekstra@outlook.com>
2015-04-25 13:18:45 +0200

    assignment submission
    changes base R file to my solution

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Inverts matrix. has a caching component to reduce unnecessary computing.
 
-## Write a short comment describing this function
+## Cache function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        cachedmatrix <- NULL
+        cachedinverse <- NULL
+        set <- function(y) 
+        {
+                x <<- y
+                cachedinverse <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(inverse) cachedinverse <<- inverse
+        getinverse <- function() cachedinverse
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## Matrix inversion function
 
-cacheSolve <- function(x, ...) {
+cacheSolve <- function(x, ...) 
+{
         ## Return a matrix that is the inverse of 'x'
+        cachedinverse <- x$getinverse()
+        if(!is.null(cachedinverse)) 
+        {
+                message("getting cached data")
+                return(cachedinverse)
+        }
+        data <- x$get()
+        cachedinverse <- solve(data, ...)
+        x$setinverse(cachedinverse)
+        message("getting non-cached data")
+        return(cachedinverse)     
 }
